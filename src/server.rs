0f389be710392e03f261b1fed5f6f1226dfcf3e2
Brian L. Troutwine <blt@postmates.com>
2016-08-08 13:48:52 -0700

    Listen for IPv4 / IPv6 connections in all sources
    This commit introduces listeners on IPv4 and IPv6 for both the
    graphite and statsd interfaces. The intention here is to support
    Go statsd clients which attempt to connect by IPv6 only, previously
    having led to silent dropping of connections.
    
    Extra threads are required in support of those systems which do not
    map IPv4 to IPv6 by default.
    
    Signed-off-by: Brian L. Troutwine <blt@postmates.com>

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,5 @@
 use std::sync::mpsc::Sender;
-use std::net::{Ipv4Addr, UdpSocket, SocketAddrV4};
+use std::net::{Ipv6Addr, UdpSocket, SocketAddrV6, SocketAddrV4, Ipv4Addr};
 use std::net::{TcpListener, TcpStream};
 use std::thread::sleep;
 use std::time::Duration;
@@ -14,7 +14,22 @@ pub enum Event {
 }
 
 /// statsd
-pub fn udp_server(chan: Sender<Event>, port: u16) {
+pub fn udp_server_v6(chan: Sender<Event>, port: u16) {
+    let addr = SocketAddrV6::new(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1), port, 0, 0);
+    let socket = UdpSocket::bind(addr).ok().expect("Unable to bind to UDP socket");
+    info!("statsd server started on ::1 {}", port);
+    let mut buf = [0; 8192];
+    loop {
+        let (len, _) = match socket.recv_from(&mut buf) {
+            Ok(r) => r,
+            Err(_) => panic!("Could not read UDP socket."),
+        };
+        let bytes = Vec::from(&buf[..len]);
+        chan.send(Event::UdpMessage(bytes)).expect("[UDP] Unable to write into chan!");
+    }
+}
+
+pub fn udp_server_v4(chan: Sender<Event>, port: u16) {
     let addr = SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), port);
     let socket = UdpSocket::bind(addr).ok().expect("Unable to bind to UDP socket");
     info!("statsd server started on 127.0.0.1:{}", port);
@@ -29,7 +44,22 @@ pub fn udp_server(chan: Sender<Event>, port: u16) {
     }
 }
 
-pub fn tcp_server(chan: Sender<Event>, port: u16) {
+pub fn tcp_server_ipv6(chan: Sender<Event>, port: u16) {
+    let addr = SocketAddrV6::new(Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1), port, 0, 0);
+    let listener = TcpListener::bind(addr).expect("Unable to bind to TCP socket");
+    info!("graphite server started on ::1 {}", port);
+    for stream in listener.incoming() {
+        let newchan = chan.clone();
+        if let Ok(stream) = stream {
+            thread::spawn(move || {
+                // connection succeeded
+                handle_client(newchan, stream)
+            });
+        }
+    }
+}
+
+pub fn tcp_server_ipv4(chan: Sender<Event>, port: u16) {
     let addr = SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), port);
     let listener = TcpListener::bind(addr).expect("Unable to bind to TCP socket");
     info!("graphite server started on 127.0.0.1:{}", port);
