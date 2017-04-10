9b0d78d6b928eba943c024896e61cd96cb7e2ca2
Blake <blake.barnett@postmates.com>
2017-04-10 10:50:11 -0700

    No need for AWS_REGION, other cleanup bits

--- docker/release/entrypoint.sh
+++ docker/release/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-export AWS_REGION="$(wget -q http://169.254.169.254/latest/meta-data/hostname -O - | cut -d'.' -f2)"
 /usr/bin/confd -onetime -backend env
 /usr/bin/cernan --config /etc/cernan.toml
