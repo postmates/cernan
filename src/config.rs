e771198f27680b7c856f1f335129614fa319b4d1
Brian L. Troutwine <blt@postmates.com>
2017-05-16 15:23:29 -0700

    Fiddle with the example configuration files
    This commit fiddles with the example configuration files on account
    of how we've changed things around the native sink. I can't seem
    to cause a failure when integration testing so, uh, I think we're
    good here.
    
    Signed-off-by: Brian L. Troutwine <blt@postmates.com>

--- src/config.rs
+++ src/config.rs
@@ -149,15 +149,16 @@ pub fn parse_args() -> Args {
 
 pub fn parse_config_file(buffer: &str, verbosity: u64) -> Args {
     let mut args = Args::default();
-    let value: toml::Value = toml::from_str(buffer).expect("could not parse config file");
+    let value: toml::Value =
+        toml::from_str(buffer).expect("could not parse config file");
 
     args.verbose = verbosity;
 
     args.data_directory = value
         .get("data-directory")
         .map(|s| {
-                 let s = s.as_str()
-                     .expect("data-directory value must be valid string");
+                 let s =
+                     s.as_str().expect("data-directory value must be valid string");
                  Path::new(s).to_path_buf()
              })
         .unwrap_or(args.data_directory);
@@ -165,8 +166,8 @@ pub fn parse_config_file(buffer: &str, verbosity: u64) -> Args {
     args.scripts_directory = value
         .get("scripts-directory")
         .map(|s| {
-                 let s = s.as_str()
-                     .expect("scripts-directory value must be valid string");
+                 let s =
+                     s.as_str().expect("scripts-directory value must be valid string");
                  Path::new(s).to_path_buf()
              })
         .unwrap_or(args.scripts_directory);
@@ -194,7 +195,8 @@ pub fn parse_config_file(buffer: &str, verbosity: u64) -> Args {
     args.filters = value
         .get("filters")
         .map(|fltr| {
-            let mut filters: HashMap<String, ProgrammableFilterConfig> = HashMap::new();
+            let mut filters: HashMap<String, ProgrammableFilterConfig> =
+                HashMap::new();
             for (name, tbl) in fltr.as_table().unwrap().iter() {
                 match tbl.get("script") {
                     Some(pth) => {
@@ -231,9 +233,12 @@ pub fn parse_config_file(buffer: &str, verbosity: u64) -> Args {
     if let Some(sinks) = value.get("sinks") {
         let sinks = sinks.as_table().expect("sinks must be in table format");
 
-        args.null = sinks
-            .get("null")
-            .map(|_| NullConfig { config_path: "sinks.null".to_string() });
+        args.null = sinks.get("null").map(|_| {
+                                              NullConfig {
+                                                  config_path: "sinks.null"
+                                                      .to_string(),
+                                              }
+                                          });
 
         args.console = sinks
             .get("console")
@@ -364,10 +369,10 @@ pub fn parse_config_file(buffer: &str, verbosity: u64) -> Args {
 
                 res.port = snk.get("port")
                     .map(|p| {
-                             p.as_integer()
+                        p.as_integer()
                                  .expect("could not parse sinks.prometheus.port") as
                              u16
-                         })
+                    })
                     .unwrap_or(res.port);
 
                 res.host = snk.get("host")
@@ -380,9 +385,9 @@ pub fn parse_config_file(buffer: &str, verbosity: u64) -> Args {
 
                 res.bin_width = snk.get("bin_width")
                     .map(|bw| {
-                             bw.as_integer()
+                        bw.as_integer()
                                  .expect("could not parse sinks.prometheus.bin_width")
-                         })
+                    })
                     .unwrap_or(res.bin_width);
 
                 res
@@ -632,11 +637,18 @@ pub fn parse_config_file(buffer: &str, verbosity: u64) -> Args {
                         res.config_path = Some(format!("sources.native.{}", name));
 
                         res.port = tbl.get("port")
-                            .map(|p| p.as_integer().expect("could not parse native port") as u16)
+                            .map(|p| {
+                                p.as_integer().expect("could not parse native port") as
+                                u16
+                            })
                             .unwrap_or(res.port);
 
                         res.ip = tbl.get("ip")
-                            .map(|p| p.as_str().expect("could not parse native ip").to_string())
+                            .map(|p| {
+                                     p.as_str()
+                                         .expect("could not parse native ip")
+                                         .to_string()
+                                 })
                             .unwrap_or(res.ip);
 
                         res.forwards = tbl.get("forwards")
@@ -654,7 +666,9 @@ pub fn parse_config_file(buffer: &str, verbosity: u64) -> Args {
 
                         assert!(res.config_path.is_some());
 
-                        native_server_config.insert(format!("sources.native.{}", name), res);
+                        native_server_config.insert(format!("sources.native.{}",
+                                                            name),
+                                                    res);
                     }
                 }
                 native_server_config
@@ -734,24 +748,6 @@ scripts-directory = "/foo/bar"
 
         assert_eq!(args.scripts_directory, dir);
     }
-
-    // #[test]
-    // fn config_file_default() {
-    //     let config = "".to_string();
-
-    //     let args = parse_config_file(config, 4);
-
-    //     assert!(!args.statsds.is_empty());
-    //     assert_eq!(args.statsds.get("sources.statsd").unwrap().port, 8125);
-    //     assert!(!args.graphites.is_empty());
-    //     assert_eq!(args.graphites.get("sources.graphite").unwrap().port, 2003);
-    //     assert_eq!(args.flush_interval, 60);
-    //     assert!(args.console.is_none());
-    //     assert!(args.null.is_none());
-    //     // assert_eq!(true, args.firehosen.is_empty());
-    //     assert!(args.wavefront.is_none());
-    //     assert_eq!(args.verbose, 4);
-    // }
 
     #[test]
     fn config_fed_receiver_sources_style() {
@@ -942,7 +938,8 @@ scripts-directory = "/foo/bar"
         assert!(args.filters.is_some());
         let filters = args.filters.unwrap();
 
-        let config0: &ProgrammableFilterConfig = filters.get("filters.collectd_scrub").unwrap();
+        let config0: &ProgrammableFilterConfig =
+            filters.get("filters.collectd_scrub").unwrap();
         let script = config0.script.clone();
         assert_eq!(script.unwrap().to_str().unwrap(),
                    "/tmp/cernan-scripts/cernan_bridge.lua");
@@ -964,7 +961,8 @@ scripts-directory = "/foo/bar"
         assert!(args.filters.is_some());
         let filters = args.filters.unwrap();
 
-        let config0: &ProgrammableFilterConfig = filters.get("filters.collectd_scrub").unwrap();
+        let config0: &ProgrammableFilterConfig =
+            filters.get("filters.collectd_scrub").unwrap();
         let script = config0.script.clone();
         assert_eq!(script.unwrap().to_str().unwrap(), "data/cernan_bridge.lua");
         assert_eq!(config0.forwards, vec!["sinks.console"]);
