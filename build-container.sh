d7e739fcbba9564b34cbc2f60f4a8d61f00758d0
Brian L. Troutwine <blt@postmates.com>
2017-06-15 13:10:40 -0700

    Correct image building
    This commit corrects our image building for dev and prod tags. We'd
    broken it when experimenting with concourse internal to Postmates
    but are moving cernan back to open alternatives. This commit should
    be the last step in that.
    
    Signed-off-by: Brian L. Troutwine <blt@postmates.com>

--- build-container.sh
+++ build-container.sh
@@ -3,7 +3,7 @@
 set -o errexit
 set -o pipefail
 set -o nounset
-# set -o xtrace
+set -o xtrace
 
 function usage() {
     echo "$0 <version>"
@@ -14,6 +14,8 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
+cargo clean
+
 VERSION="${1}"
 confd_version=0.11.0
 confd_url="https://github.com/kelseyhightower/confd/releases/download/v${confd_version}/confd-${confd_version}-linux-amd64"
@@ -22,9 +24,8 @@ mkdir -p target/bindir/
 chmod +x target/bindir/confd-bin
 
 docker build -t cernan-build -f docker/build/Dockerfile .
-CONTAINER_ID=$(docker create cernan-build)
-# Remove 'cernan' from the build image 
-docker container cp ${CONTAINER_ID}:/source/target/release/cernan target/bindir/
-docker rm ${CONTAINER_ID}
-docker build --build-arg EXE_ROOT=target/bindir -t quay.io/postmates/cernan:latest -t quay.io/postmates/cernan:${VERSION} -f docker/release/Dockerfile .
-rm target/bindir/cernan
+docker run -v $(pwd):/source -w /source -it cernan-build cargo build --release
+cp target/release/cernan target/bindir/
+docker build -t quay.io/postmates/cernan:latest -t "quay.io/postmates/cernan:$VERSION" -f docker/release/Dockerfile .
+docker push "quay.io/postmates/cernan:$VERSION"
+docker push "quay.io/postmates/cernan:latest"
