4c62eba1d6b67ea10fa0f61cbce0f70981c044f4
Ivan Glushkov <ivan@postmates.com>
2017-06-27 17:39:54 +0300

    rename to a more proper name error -> error_bound

--- src/metric/telemetry.rs
+++ src/metric/telemetry.rs
@@ -16,7 +16,7 @@ pub struct Telemetry {
     pub tags: sync::Arc<TagMap>,
     pub timestamp: i64, // seconds, see #166
     pub timestamp_ns: u64,
-    pub error: f64,
+    pub error_bound: f64,
 }
 
 #[derive(PartialEq, Debug, Serialize, Deserialize, Clone)]
@@ -30,7 +30,7 @@ pub struct Value {
     kind: ValueKind,
     single: Option<f64>,
     many: Option<CKMS<f64>>,
-    error: f64,
+    error_bound: f64,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone, PartialEq, PartialOrd, Eq, Hash)]
@@ -82,16 +82,16 @@ impl PartialOrd for Telemetry {
 
 impl Default for Telemetry {
     fn default() -> Telemetry {
-        let default_error = 0.001;
+        let default_error_bound = 0.001;
         Telemetry {
             name: String::from(""),
-            value: Value::new(Default::default(), default_error),
+            value: Value::new(Default::default(), default_error_bound),
             persist: false,
             aggr_method: AggregationMethod::Summarize,
             tags: sync::Arc::new(TagMap::default()),
             timestamp: time::now(),
             timestamp_ns: time::now_ns(),
-            error: default_error,
+            error_bound: default_error_bound,
         }
     }
 }
@@ -114,10 +114,10 @@ impl Telemetry {
     /// assert_eq!(m.name, "foo");
     /// assert_eq!(m.value(), Some(1.1));
     /// ```
-    pub fn new<S>(name: S, value: f64, error: f64) -> Telemetry
+    pub fn new<S>(name: S, value: f64, error_bound: f64) -> Telemetry
         where S: Into<String>
     {
-        let val = Value::new(value, error);
+        let val = Value::new(value, error_bound);
         Telemetry {
             aggr_method: AggregationMethod::Summarize,
             name: name.into(),
@@ -126,7 +126,7 @@ impl Telemetry {
             timestamp_ns: time::now_ns(),
             value: val,
             persist: false,
-            error: error,
+            error_bound: error_bound,
         }
     }
 
@@ -243,7 +243,7 @@ impl Telemetry {
     /// assert_eq!(m.value(), Some(10.10));
     /// ```
     pub fn set_value(mut self, value: f64) -> Telemetry {
-        self.value = Value::new(value, self.error);
+        self.value = Value::new(value, self.error_bound);
         self
     }
 
@@ -438,12 +438,12 @@ impl AddAssign for Value {
 }
 
 impl Value {
-    fn new(value: f64, error: f64) -> Value {
+    fn new(value: f64, error_bound: f64) -> Value {
         Value {
             kind: ValueKind::Single,
             single: Some(value),
             many: None,
-            error: error
+            error_bound: error_bound
         }
     }
 
@@ -457,7 +457,7 @@ impl Value {
     fn insert(&mut self, value: f64) -> () {
         match self.kind {
             ValueKind::Single => {
-                let mut ckms = CKMS::new(self.error);
+                let mut ckms = CKMS::new(self.error_bound);
                 ckms.insert(self.single.expect("NOT SINGLE IN METRICVALUE INSERT"));
                 ckms.insert(value);
                 self.many = Some(ckms);
