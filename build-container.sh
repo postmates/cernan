ffbc1129b24290618a2c042e9ea875f8ad29d13a
Brian L. Troutwine <brian@troutwine.us>
2017-09-14 13:51:58 -0700

    Spruce up the cernan release build (#315)
    * Spruce up the cernan release build
    
    We used to build releases inside Alpine Linux. This worked out okay for
    a while but the rust version there has gotten very out of date as of
    this writing. We're using ekidd/rust-musl-builder now, a Debian base
    that'll crank out musl binaries. We're still targeting Alpine as the
    deployment image.
    
    Signed-off-by: Brian L. Troutwine <blt@postmates.com>
    
    * Further fixes and fiddling
    
    With this commit it's now possible to build cernan docker containers
    again. Wee
    
    Signed-off-by: Brian L. Troutwine <blt@postmates.com>

--- build-container.sh
+++ build-container.sh
@@ -17,15 +17,10 @@ fi
 cargo clean
 
 VERSION="${1}"
-confd_version=0.11.0
-confd_url="https://github.com/kelseyhightower/confd/releases/download/v${confd_version}/confd-${confd_version}-linux-amd64"
-mkdir -p target/bindir/
-[ -f target/bindir/confd-bin ] || curl -L "$confd_url" > target/bindir/confd-bin
-chmod +x target/bindir/confd-bin
 
+mkdir -p target/bindir
 docker build -t cernan-build -f docker/build/Dockerfile .
 docker run -v $(pwd):/source -w /source -it cernan-build cargo build --release
-cp target/release/cernan target/bindir/
 docker build -t quay.io/postmates/cernan:latest -t "quay.io/postmates/cernan:$VERSION" -f docker/release/Dockerfile .
 docker push "quay.io/postmates/cernan:$VERSION"
 docker push "quay.io/postmates/cernan:latest"
