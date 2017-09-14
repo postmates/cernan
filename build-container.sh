9dc898fe93c30e191727acdb16b3733e9b7acf9f
Brian L. Troutwine <blt@postmates.com>
2017-09-14 13:32:29 -0700

    Further fixes and fiddling
    With this commit it's now possible to build cernan docker containers
    again. Wee
    
    Signed-off-by: Brian L. Troutwine <blt@postmates.com>

--- build-container.sh
+++ build-container.sh
@@ -18,9 +18,9 @@ cargo clean
 
 VERSION="${1}"
 
+mkdir -p target/bindir
 docker build -t cernan-build -f docker/build/Dockerfile .
 docker run -v $(pwd):/source -w /source -it cernan-build cargo build --release
-cp target/x86_64-unknown-linux-musl/release/cernan target/bindir/
 docker build -t quay.io/postmates/cernan:latest -t "quay.io/postmates/cernan:$VERSION" -f docker/release/Dockerfile .
 docker push "quay.io/postmates/cernan:$VERSION"
 docker push "quay.io/postmates/cernan:latest"
